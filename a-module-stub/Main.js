a52dfec5d24d4482253915b7455e412e2167aa3e
TheGiddyLimit <TheGiddyLimit@5etools.com>
2024-03-03 21:55:24 +0000

    feat: add libwrapper sample

--- a-module-stub/Main.js
+++ a-module-stub/Main.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+Hooks.on("init", () => {
+	libWrapper.register(
+		"a-module-stub",
+		"TextEditor._onClickContentLink",
+		async (fn, ...args) => {
+			return fn(...args);
+		},
+		"MIXED",
+	);
+
+	document.body.addEventListener("click", () => {
+		// waste some time
+		const num = Array.from({length: 1_000_000}, (_, i) => i).reduce((a, b) => a + b, 0);
+		console.log(`click counted to ${num}`);
+	});
+})
